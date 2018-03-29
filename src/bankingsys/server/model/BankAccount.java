9dc8ddddef26ad6d48134852acdd980c6d64e426
Lynisiki <sumdreamin@gmail.com>
2018-03-29 16:34:58 +0800

    Bank account initial

--- src/bankingsys/server/model/BankAccount.java
+++ src/bankingsys/server/model/BankAccount.java
@@ -4,4 +4,48 @@ package bankingsys.server.model;
  * Created by koallen on 29/3/18.
  */
 public class BankAccount {
+    private int accountNumer;
+    private String name;
+    private String password;
+
+    private float balance;
+
+    public BankAccount(int accountNumer, String name, String password, float balance) {
+        this.accountNumer = accountNumer;
+        this.name = name;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public int getAccountNumer() {
+        return accountNumer;
+    }
+
+    public void setAccountNumer(int accountNumer) {
+        this.accountNumer = accountNumer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
 }
