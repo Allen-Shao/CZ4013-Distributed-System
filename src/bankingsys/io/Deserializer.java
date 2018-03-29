de1e7a2f47f01d19304783cc18494759092b881f
Siyuan Liu <me@shawnliu.me>
2018-03-29 17:40:43 +0800

    implement serialization support

--- src/bankingsys/io/Deserializer.java
+++ src/bankingsys/io/Deserializer.java
@@ -4,4 +4,38 @@ package bankingsys.io;
  * Created by koallen on 29/3/18.
  */
 public class Deserializer {
+    private byte[] buffer;
+    private int bufferPosition;
+
+    public Deserializer(byte[] inputBuffer) {
+        buffer = inputBuffer;
+        bufferPosition = 0;
+    }
+
+    public int readInt() {
+        int val = (buffer[bufferPosition] << 24) & 0xFF000000 |
+                (buffer[bufferPosition + 1] << 16) & 0x00FF0000 |
+                (buffer[bufferPosition + 2] << 8) & 0x0000FF00 |
+                buffer[bufferPosition + 3] & 0x000000FF;
+        bufferPosition += 4;
+        return val;
+    }
+
+    public float readFloat() {
+        int val = readInt();
+        return Float.intBitsToFloat(val);
+    }
+
+    public String readString() {
+        int stringLength = readInt();
+        byte[] stringBuffer = new byte[stringLength];
+        System.arraycopy(buffer, bufferPosition, stringBuffer, 0, stringLength);
+        String str = new String(stringBuffer);
+        bufferPosition += stringLength;
+        return str;
+    }
+
+    //private boolean isWithinBound(int len) {
+    //    return bufferPosition + len < buffer.length ? true : false;
+    //}
 }
--- src/bankingsys/io/Serializer.java
+++ src/bankingsys/io/Serializer.java
@@ -4,4 +4,54 @@ package bankingsys.io;
  * Created by koallen on 29/3/18.
  */
 public class Serializer {
+    private byte[] buffer; // maintains the byte array
+    private int bufferLength; // maintains the current buffer position
+
+    public Serializer() {
+        buffer = new byte[4];
+        bufferLength = 0;
+    }
+
+    public void writeInt(int input) {
+        extendBuffer(4);
+        buffer[bufferLength] = (byte) (input >> 24);
+        buffer[bufferLength + 1] = (byte) (input >> 16);
+        buffer[bufferLength + 2] = (byte) (input >> 8);
+        buffer[bufferLength + 3] = (byte) (input);
+        bufferLength += 4;
+    }
+
+    public void writeFloat(float input) {
+        int inputInInt = Float.floatToIntBits(input);
+        writeInt(inputInInt);
+    }
+
+    public void writeString(String input) {
+        writeInt(input.length());
+        extendBuffer(input.length());
+        byte[] stringByte = input.getBytes();
+        System.arraycopy(stringByte, 0, buffer, bufferLength, input.length());
+        bufferLength += input.length();
+    }
+
+    public byte[] getBuffer() {
+        return buffer;
+    }
+
+    public int getBufferLength() {
+        return bufferLength;
+    }
+
+    /**
+     * Increase the buffer size if necessary
+     * @param len
+     */
+    private void extendBuffer(int len) {
+        int newLength = bufferLength + len;
+        if (newLength > buffer.length) {
+            byte[] newBuffer = new byte[newLength];
+            System.arraycopy(buffer, 0, newBuffer, 0, buffer.length);
+            buffer = newBuffer;
+        }
+    }
 }
