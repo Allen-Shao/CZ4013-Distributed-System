2be87b85b870fa02ab84eb4e370e6387c50d3f35
SHAO YIYANG <shaoyiyang312@hotmail.com>
2018-03-30 15:33:04 +0800

    command line interface

--- src/bankingsys/client/RequestSender.java
+++ src/bankingsys/client/RequestSender.java
@@ -1,11 +1,20 @@
 package bankingsys.client;
 
+import java.util.Scanner;
+
 /**
  * Created by koallen on 29/3/18.
  */
 public class RequestSender {
 
-    public static void main(args[]) {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print(">>> ");
+            String command = sc.nextLine();
+            System.out.println(command);
+        }
+
 
     }
 }
