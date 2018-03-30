7849029992329f0e99b5e39407d1d00266c72dca
SHAO YIYANG <shaoyiyang312@hotmail.com>
2018-03-30 16:32:54 +0800

    parse arguments to service request

--- src/bankingsys/client/RequestSender.java
+++ src/bankingsys/client/RequestSender.java
@@ -1,10 +1,14 @@
 package bankingsys.client;
 
+import bankingsys.message.ServiceRequest;
+import bankingsys.server.model.BankAccount;
+
 import java.util.Scanner;
 
 /**
  * Created by koallen on 29/3/18.
  */
+
 public class RequestSender {
 
     public static void main(String args[]) {
@@ -12,7 +16,87 @@ public class RequestSender {
         while (true) {
             System.out.print(">>> ");
             String command = sc.nextLine();
+
+            // Create Name Password CurrencyType Amount
+            // Close Name AccountNumber Password
+            // Deposit Name AccountNumber Password CurrencyType Amount
+            // Withdraw Name AccountNumber Password CurrencyType Amount
+            // Moniter
+            // Check Name AccountNumber Password
+            // Transfer Name AccountNumber Password TargetAccount Amount
+
             System.out.println(command);
+            String[] commandSplits = command.split(" ");
+            String commandType = commandSplits[0];
+            System.out.println(commandType);
+
+            ServiceRequest request;
+            switch (commandType){
+                case "Create":
+                    request = new ServiceRequest(
+                            'b',
+                            commandSplits[1],
+                            null,
+                            commandSplits[2],
+                            Float.parseFloat(commandSplits[4]),
+                            null,
+                            BankAccount.Currency.valueOf(commandSplits[3]));
+                    break;
+                case "Close":
+                    request = new ServiceRequest(
+                            'a',
+                            commandSplits[1],
+                            Integer.parseInt(commandSplits[2]),
+                            commandSplits[3],
+                           null,
+                            null,
+                            null);
+                    break;
+                case "Deposit":
+                    request = new ServiceRequest(
+                            'e',
+                            commandSplits[1],
+                            Integer.parseInt(commandSplits[2]),
+                            commandSplits[3],
+                            -Float.parseFloat(commandSplits[5]),
+                            null,
+                            BankAccount.Currency.valueOf(commandSplits[4]));
+                    break;
+                case "Withdraw":
+                    request = new ServiceRequest(
+                            'e',
+                            commandSplits[1],
+                            Integer.parseInt(commandSplits[2]),
+                            commandSplits[3],
+                            Float.parseFloat(commandSplits[5]),
+                            null,
+                            BankAccount.Currency.valueOf(commandSplits[4]));
+                    break;
+                case "Moniter":
+                    break;
+                case "Check":
+                    request = new ServiceRequest(
+                            'd',
+                            commandSplits[1],
+                            Integer.parseInt(commandSplits[2]),
+                            commandSplits[3],
+                            null,
+                            null,
+                           null);
+                    break;
+                case "Transfer":
+                    request = new ServiceRequest(
+                            'f',
+                            commandSplits[1],
+                            Integer.parseInt(commandSplits[2]),
+                            commandSplits[3],
+                            Float.parseFloat(commandSplits[5]),
+                            Integer.parseInt(commandSplits[4]),
+                            null);
+                    break;
+            }
+
+
         }
 
 
