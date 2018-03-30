7349d37a0c4ab433276bb9f916163dd69fc16418
Siyuan Liu <me@shawnliu.me>
2018-03-30 16:24:19 +0800

    update all handlers

--- src/bankingsys/server/handler/AccountCancellationHandler.java
+++ src/bankingsys/server/handler/AccountCancellationHandler.java
@@ -2,11 +2,18 @@ package bankingsys.server.handler;
 
 import bankingsys.message.ServiceRequest;
 import bankingsys.message.ServiceResponse;
+import bankingsys.server.model.BankAccount;
+
+import java.util.HashMap;
 
 /**
  * Created by koallen on 29/3/18.
  */
-public class AccountCancellationHandler implements ServiceHandler {
+public class AccountCancellationHandler extends ServiceHandler {
+    public AccountCancellationHandler(HashMap<Integer, BankAccount> accounts) {
+        super(accounts);
+    }
+
     @Override
     public ServiceResponse handleRequest(ServiceRequest request) {
         return null;
